b026c1e0b853cdce741f7e0ec8e75dc9ee56be26
DavidHristov <davidkl@abv.bg>
2025-01-22 09:40:17 +0200

    dfsf

---
+++ WebShopApp/WebShopApp/Controllers/OrderController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Security.Claims;
+
+using WebShopApp.Core.Contracts;
+using WebShopApp.Infrastructure.Data.Domain;
+using WebShopApp.Models.Order;
+
+namespace WebShopApp.Controllers
+{
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly IOrderService _orderService;
+
+        public OrderController(IProductService productService, IOrderService orderService)
+        {
+            _productService = productService;
+            _orderService = orderService;
+        }
+
+        public ActionResult Create(int id)
+        {
+            Product product = _productService.GetPRoductById(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            OrderCreateVM order = new OrderCreateVM()
+            {
+                ProductId = product.Id,
+                ProductName = product.ProductName,
+                QuantityInStock = product.Quantity,
+                Price = product.Price,
+                Discount = product.Discount,
+                Picture = product.Picture,
+
+            };
+
+            return View(order);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(OrderCreateVM bindingModel)
+        {
+            string currentUserId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var product = this._productService.GetPRoductById(bindingModel.ProductId);
+
+            if (currentUserId == null || product == null || product.Quantity < bindingModel.Quantity || product.Quantity == 0)
+            {
+                return RedirectToAction("Denied", "Order");
+            }
+            if (ModelState.IsValid)
+            {
+                _orderService.Create(bindingModel.ProductId, currentUserId, bindingModel.Quantity);
+            }
+
+            return this.RedirectToAction("Index", "Product"); 
+        }
+
+        public ActionResult Denied()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebShopApp/WebShopApp/Models/Order/OrderIndexVM.cs
@@ -0,0 +1,14 @@
+﻿namespace WebShopApp.Models.Order
+{
+    public class OrderIndexVM
+    {
+        public int Id { get; set;}
+        public string OrderDate { get; set; } = null!;
+        public string UserId { get; set; } = null!;
+        public string User { get; set; } = null!;
+
+        public int ProductId { get; set;}
+        public string Product { get; set; } = null!;
+        public string Picture {  get; set; } = null!;
+    }
+}
